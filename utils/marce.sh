982bbc0039425be308fd15bead6f134b567fa8dc
OptiJuegos <94207445+OptiJuegos@users.noreply.github.com>
2024-12-07 22:48:04 -0300

    Update marce.sh

--- utils/marce.sh
+++ utils/marce.sh
@@ -1,1 +1,1 @@
-apt install tigervnc-standalone-server & vncserver -SecurityTypes None -geometry 1366x768 && ./marce/utils/noveneco_proxy --vnc localhost:5901 --listen 0.0.0.0:7860
+vncserver -SecurityTypes None -geometry 1366x768 && ./marce/utils/noveneco_proxy --vnc localhost:5901 --listen 0.0.0.0:7860
