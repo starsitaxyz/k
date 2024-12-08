98538a910c20dea8ea16c3b9530fa5dbca14138a
OptiJuegos <94207445+OptiJuegos@users.noreply.github.com>
2024-12-08 14:02:23 -0300

    Update marce.sh

--- utils/marce.sh
+++ utils/marce.sh
@@ -1,1 +1,1 @@
-vncserver -SecurityTypes None -geometry 1366x768 && ./marce/utils/noveneco_proxy --vnc localhost:5901 --listen 0.0.0.0:7860
+vncserver -SecurityTypes None -geometry 1366x768 && ./marce/utils/nomarce_proxy --vnc localhost:5901 --listen 0.0.0.0:7860
