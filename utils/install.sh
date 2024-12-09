e9f1095e375e5bdc04d1fba5a5110e4cc011a682
OptiJuegos <94207445+OptiJuegos@users.noreply.github.com>
2024-12-09 13:18:01 -0300

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -1,1 +1,2 @@
-apt install tigervnc-standalone-server -y
+useradd -m -u 1000 user
+apt install tigervnc-standalone-server openjdk-17-jdk xfce4-terminal lxde aqemu sudo curl wget aria2 qemu-system-x86 htop chromium screen python3-pip python3-websockify python3 -y
