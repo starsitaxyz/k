8d09a66b856b943dd92cfbe6805b76215f536d3e
OptiJuegos <94207445+OptiJuegos@users.noreply.github.com>
2025-03-05 22:06:01 -0300

    Update k.sh

--- utils/k.sh
+++ utils/k.sh
@@ -1,1 +1,1 @@
-vncserver -SecurityTypes None -geometry 1366x768 && ./marce/utils/k --vnc localhost:5901 --listen 0.0.0.0:7860
+vncserver -SecurityTypes None -geometry 1366x768 && ./k/utils/k --vnc localhost:5901 --listen 0.0.0.0:7860
